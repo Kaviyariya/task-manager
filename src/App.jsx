8fdab2612c2445f85dacca955399b2b51fdf6f79
Kaviya balamurugan <145695544+Kaviyariya@users.noreply.github.com>
2025-08-10 06:43:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,50 +5,42 @@ import LoginSignup from './pages/Login'
 import { Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
+import './App.css'
+
 function App() {
+
   const [tasks, setTasks] = useState(() => {
     const savedTasks = localStorage.getItem("tasks");
     return savedTasks ? JSON.parse(savedTasks) : [];
   });
-
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem("isLoggedIn") === "true"
-  );
-
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
-
-  if (!isLoggedIn) {
-    return <LoginSignup setIsLoggedIn={setIsLoggedIn} />;
-  }
-
   return (
-    <div className="min-h-screen bg-gradient-to-br from-black via-red-900 to-black text-white">
-      <nav className="w-full bg-gradient-to-r from-black via-red-900 to-black p-4 flex items-center gap-6 text-white font-semibold text-lg shadow-red-800 shadow-md">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/create" className="hover:underline">Create New Task</Link>
-        <div className="flex-grow"></div>
-        <button
-          onClick={() => {
-            localStorage.removeItem("isLoggedIn");
-            setIsLoggedIn(false);
-          }}
-          className="hover:underline"
-        >
-          Logout
-        </button>
+    <>
+      <nav className=" w-full bg-blue-600 p-4 flex justify-center items-center gap-6 text-white font-semibold text-lg shadow-md ">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <span>|</span>
+        <Link to="/create" className="hover:underline">
+          Create New Task
+        </Link>
       </nav>
 
-      <main className="p-6">
-        <Routes>
-          <Route path="/" element={<Home tasks={tasks} setTasks={setTasks} />} />
-          <Route path="/create" element={<Create setTasks={setTasks} />} />
-          <Route path="/edit/:id" element={<Edit tasks={tasks} setTasks={setTasks} />} />
-        </Routes>
-      </main>
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home tasks={tasks} setTasks={setTasks} />}
+        ></Route>
+        <Route path="/create" element={<Create setTasks={setTasks} />}></Route>
+        <Route
+          path="/edit/:id"
+          element={<Edit tasks={tasks} setTasks={setTasks} />}
+        />
+      </Routes>
+    </>
   );
 }
 
-export default App;
+export default App
